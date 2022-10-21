8c98363db16a4d2cd9b9f798139f33421ee1bb43
Luke Fitzpatrick <lukefitzpatrick@Lukes-MacBook-Pro.local>
2022-10-20 22:42:00 -0300

    update code

--- maincode.ino
+++ maincode.ino
@@ -3,11 +3,13 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
+#include <FirebaseESP8266.h>
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
 
-#ifndef STASSID
-#define STASSID "Type-P"
-#define STAPSK  "6@Z*vW5a"
-#endif
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+#include "definitions.h"
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
@@ -15,9 +17,25 @@ const char* password = STAPSK;
 String wifiSsid="";
 String wifiPassword;
 
-String token;
-
 ESP8266WebServer server(80);
+
+// Define Firebase Data object
+FirebaseData stream;
+FirebaseData fbdo;
+
+FirebaseJson dataToApp;
+FirebaseJson dataFromApp;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+long currentSec=0;
+
+// ############################################################## Recycle Data #####################################################
+
+unsigned int timeDefined=0;
 
 void setup(void) {
   Serial.begin(115200);
@@ -30,33 +48,57 @@ void setup(void) {
   Serial.println(WiFi.softAPIP());
   // Serial.println("HTTP server started");
   apiRoutes();
+  
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  /* In case the certificate data was used  */
+  config.cert.data = rootCACert;
+
+  // Or custom set the root certificate for each FirebaseData object
+  fbdo.setCert(rootCACert);
+
+  Firebase.reconnectWiFi(true);
+  Firebase.setDoubleDigits(5);
+  Firebase.setStreamCallback(stream, streamCallback, streamTimeoutCallback);
+  Firebase.begin(&config, &auth);
+
   server.begin();
 }
 
+// ######################################################### LOOP ##########################################################
+
+
 void loop(void) {
   server.handleClient();
+  if(Firebase.ready() && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    currentSec++;
+  }
 }
 
-// ***************************** Network *********************************
+// ####################################################### FUNCTIONS ########################################################
+
+// ********************************************************* Network **************************************************************
 
 void apiRoutes(){
   server.on("/", HTTP_GET, returnConnection);
-  // server.sendHeader(F("Access-Control-Max-Age"), F("900"));
-  // server.sendHeader(F("Keep-Alive"), F("timeout=5000"));
-  // server.on(F("/token"), HTTP_OPTIONS, setCrossOrigin);
-  server.on(F("/token"), HTTP_POST, setToken);
   server.on(F("/scan"),HTTP_GET, getScanNetworks);
   server.on(F("/login"), HTTP_POST, setCredentials);
+  server.on(F("/firebase"),HTTP_GET, getFirebaseConnection);
 }
-
-void setCrossOrigin(){
-    server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
-    server.sendHeader(F("Access-Control-Max-Age"), F("600"));
-    server.sendHeader(F("Access-Control-Allow-Methods"), F("PUT,POST,GET,OPTIONS"));
-    server.sendHeader(F("Access-Control-Allow-Headers"), F("*"));
-    server.sendHeader(F("Access-control-Allow-Credentials"), F("false"));
-    // server.send(204);
-};
 
 void returnConnection(){
   DynamicJsonDocument doc(1024);
@@ -65,23 +107,6 @@ void returnConnection(){
   String jsonSerialized;
   serializeJson(obj, jsonSerialized);
   server.send(202, "application/json", jsonSerialized);  
-}
-
-void setToken(){
-  DynamicJsonDocument doc(1024);
-  JsonObject obj = doc.createNestedObject();
-  token = server.arg("token");
-  Serial.println(server.args());
-  Serial.println(token);
-    Serial.println(server.argName(0));
-  if(token!=""){
-    obj["token_paried"] = true;
-  }else{
-    obj["token_paried"] = false;
-  }
-  String jsonSerialized;
-  serializeJson(obj, jsonSerialized);
-  server.send(202, "application/json", jsonSerialized);
 }
 
 void getScanNetworks() {
@@ -121,8 +146,6 @@ void getScanNetworks() {
       Serial.print(rssi);
       Serial.print(" - Security: ");
       Serial.println(security);
-
-      
       obj["ssid"] = ssid;
       obj["strength"] = rssi;
       obj["security"] = security == "none" ? false : true;
@@ -151,6 +174,7 @@ void setCredentials(){
   JsonObject obj = doc.createNestedObject();
 
   if(WiFi.waitForConnectResult()==WL_CONNECTED){
+      connectToFirebase();
       obj["connected"] = true;
       obj["status"] = "CONNECTED";
   }else if (WiFi.waitForConnectResult()==WL_NO_SSID_AVAIL){
@@ -169,4 +193,56 @@ void setCredentials(){
   String jsonSerialized;
   serializeJson(obj, jsonSerialized);
   server.send(202, "application/json", jsonSerialized);  
+}
+
+void getFirebaseConnection(){
+  DynamicJsonDocument doc(1024);
+  JsonObject obj = doc.createNestedObject();
+  dataToApp.add("func", "OVEN_CONNECTED");
+  dataToApp.add("val", true);
+  if(Firebase.ready()){    
+    if(Firebase.setJSON(fbdo, PATH_TO_APP, dataToApp)){    
+      obj["firebase_connected"] = true;
+    }else{
+      obj["firebase_connected"] = false;
+      // connectToFirebase();
+    }
+  }else{
+      obj["firebase_connected"] = false;
+  }
+  dataToApp.clear();
+  String jsonSerialized;
+  serializeJson(obj, jsonSerialized);
+  server.send(202, "application/json", jsonSerialized);  
+}
+
+// ******************************************************* FIREBASE ************************************************************
+
+void connectToFirebase(){  
+  stream.setBSSLBufferSize(2048 /* Rx in bytes, 512 - 16384 */, 512 /* Tx in bytes, 512 - 16384 */);
+  if (!Firebase.beginStream(stream, PATH_FROM_APP)){
+    Serial.printf("sream begin error, %s\n\n", stream.errorReason().c_str());
+  }else{
+    Serial.println("stream ready");
+  }
+}
+
+void streamCallback(StreamData data)
+{
+  Serial.printf("value:");
+  Serial.printf(data.payload().c_str());
+  dataFromApp = data.payload();
+}
+
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout){
+    
+    Serial.println("stream timed out, resuming...\n");
+  }
+
+  if (!stream.httpConnected()){
+
+    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
+  }
 }
